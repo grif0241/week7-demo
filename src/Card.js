5656b1145fdcfd2b8c4c8a1d5cd43cb96b862a81
grif0241 <grif0241@algonquinlive.com>
2022-10-19 20:11:24 -0400

    add rating

--- src/Card.js
+++ src/Card.js
@@ -3,6 +3,7 @@ import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 // import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import Rating from '@mui/material/Rating';
 
 function Card( {data} ) {
   if(data.length === 0) { // .length can only check the length of an empty array; so when setting the state variable it must be empty array
@@ -15,6 +16,7 @@ function Card( {data} ) {
           <Grid item xs={12} sm={6} md={4} key={item.id}> 
             <Paper elevation={3} className="paper">
               <img className="image" src={item.image} alt={item.title}></img>
+              <Rating name="disabled" value={item.rating.rate} disabled />
               <Typography variant="body1" gutterBottom> {item.title} </Typography>
             </Paper>
           </Grid>
